ffc3b7cfa31855aa43b98d241cdfd9dbe06ab94b
Muhammad Usama Siddiqui <usiddiqui14@gmail.com>
2018-12-04 12:27:19 +0500

    Second Version

--- talko/models.py
+++ talko/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-def __str__(self):
-    return self.user.usrname
-
-
 class Message(models.Model):
     text = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
